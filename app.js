b30e62cf8cbf0b883dd803a1b897c905eacc0b39
storm-sandy <sanleosmile@gmail.com>
2026-06-19 16:38:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/service-worker.js");
+  navigator.serviceWorker.register("/sw.js");
 }
   navigator.serviceWorker.addEventListener('message', event => {
     if (event.data.type === 'UPDATE_READY') {
