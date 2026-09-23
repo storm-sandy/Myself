d384fdecdac4baba0ee2fd58dd6615675c2f89af
storm-sandy <sanleosmile@gmail.com>
2026-09-23 21:45:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ function autoCacheAllFiles() {
   const filesToCache = new Set([
     "./",
     "./index.html",
-    "/Myself/util/home.html",
-    "/Myself/style/style.css",
-    "/Myself/js/app.js",
-    "/Myself/js/manifest.json",
+    "./home.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json",
     "./icon.png"
   ]);
 
